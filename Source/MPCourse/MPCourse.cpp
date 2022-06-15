2fa89b1b106cd407e2732a5d731bd09f36c34d36
kseon12 <kearir12@gmail.com>
2022-06-15 13:44:17 +0300

    Project setup

---
+++ Source/MPCourse/MPCourse.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MPCourse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MPCourse, "MPCourse" );
---
+++ Source/MPCourse/MPCourseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MPCourseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MPCOURSE_API AMPCourseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
